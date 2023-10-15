696e4c9e7b1acbf93e32eb89d9e0b4c82572bea1
Diego Perez da Costa <diegoperezdacosta@MacBook-Pro-de-Diego.local>
2023-10-15 17:04:16 -0300

    Add: Random gems

--- src/helpers/genereteGame.ts
+++ src/helpers/genereteGame.ts
@@ -7,11 +7,12 @@ export function generateGame(
   bombCount: number
 ): Option[] {
   const options: Option[] = [];
+  const randomGem = Math.random() < 0.4;
 
   for (let i = 0; i < optionsCount; i++) {
     if (i < bombCount) {
       options.push({ index: i, ...bombOption });
-    } else if (i < bombCount + 1) {
+    } else if (randomGem && i < bombCount + 1) {
       options.push({ index: i, ...gemOption });
     } else {
       options.push({ index: i, ...cashOption });
